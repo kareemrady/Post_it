157659378e5439b47d013f7b234d31ae891cad57
Kareem Rady <kareem_rady@yahoo.com>
2013-07-07 20:45:56 -0400

    Updated Code, added Edit & update functons & created partial form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,18 +9,33 @@ class PostsController < ApplicationController
   end
 
   def new
+  	@post = Post.new
 
   end
 
   def create
-
+  	@post = Post.new(params[:post].permit!)
+  	if @post.save
+  		redirect_to root_path, notice: "You have successfully created a post!"
+  	else
+  		render :new
+  	end
   end
 
   def edit
+    @post = Post.find(params[:id])
 
   end
 
   def update
-
+    
+   @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post].permit!)
+      redirect_to root_path, notice: "You have successfully updated your post!"
+    else
+      render :edit
+    end
   end
+
+  
 end
