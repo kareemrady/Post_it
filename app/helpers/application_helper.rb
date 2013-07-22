85a143eec4ae3bae1a24ae37133bb094e34dd79e
Kareem Rady <kareem_rady2yahoo.com>
2013-07-21 22:36:29 -0400

    Added Helper methods for time & url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def date_time(dt)
+		dt.strftime("%a,%e %b %Y %H:%m:%S %p %Z")
+	end
+
+	def url_fixer(url)
+		url.starts_with?("http://") ? url : "http://#{url}"
+	end
 end
